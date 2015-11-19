5b5b3ad4a09a34861036fc8b2aa0385a959d3b9b
Jasonnor <ja387881@yahoo.com.tw>
2015-11-19 15:30:09 +0800

    Hello Code

---
+++ NovelPretender/MainWindow.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace NovelPretender
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextLabel = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextLabel
+            // 
+            this.TextLabel.AutoSize = true;
+            this.TextLabel.Location = new System.Drawing.Point(12, 23);
+            this.TextLabel.Name = "TextLabel";
+            this.TextLabel.Size = new System.Drawing.Size(226, 20);
+            this.TextLabel.TabIndex = 0;
+            this.TextLabel.Text = "Please select a novel file (.txt)";
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Start.Location = new System.Drawing.Point(252, 21);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.buttonGetFile_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 66);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.TextLabel);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "MainWindow";
+            this.Text = "Novel Pretender";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TextLabel;
+        private System.Windows.Forms.Button Start;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,19 @@
+# Novel Pretender
+
+Novel Pretender can insert random C# or PHP code to a text file (which may be novel or article) paragraphs, and output to html, making it look like a professional or work site.
+
+## Before
+
+![Before](Example/Before.png)
+
+## After
+
+![After](Example/After.png)
+
+## Contributing
+
+This is a simple C# Windows Forms Application, please feel free to use it if you are interested in fixing issues and contributing directly to the code base.
+
+## License
+
+Novel Pretender is released under the [MIT](LICENSE.txt) license.
