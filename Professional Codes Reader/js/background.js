1f6bd767abd5fac90800333ea9a0fda6009b1641
Jason Wu <wujason810@gmail.com>
2017-06-07 17:12:28 +0800

    Empty the autoRun domain

--- Professional Codes Reader/js/background.js
+++ Professional Codes Reader/js/background.js
@@ -5,13 +5,7 @@ var url;
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (typeof changeInfo.url !== 'undefined' && url != changeInfo.url) {
       url = changeInfo.url;
-      var autoRun = [
-        'www.uukanshu.com',
-        'www.infoq.com',
-        '36kr.com',
-        'acgspace.wsfun.com',
-        'gamer.com.tw'
-      ];
+      var autoRun = [];
       for (var i = 0; i < autoRun.length; i++)
         if (url.indexOf(autoRun[i]) > -1) {
           chrome.tabs.executeScript(tabId, {file: 'js/pretender.js'});
